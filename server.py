754cae6d1081fdde0ff9f102da81a9eb8d919959
MinkyoungCho <chomgopq263@gmail.com>
2022-11-18 19:13:43 -0500

    upload server code

---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+import time
+
+lidar_frequency = 1 # fix this to 1 to measure the delay for 1 point cloud frame
+HOST = "141.212.108.156" 
+PORT = 8000
+
+try:
+    while True:
+
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('[1] socked created. waiting for connecting to server...')
+
+        soc.bind((HOST, PORT))
+        soc.listen()
+        print('[2] connected to the server.')
+
+        print ("[3] waiting client..")
+        con, addr = soc.accept()
+        fragments = []
+
+        for i in range(lidar_frequency):
+            chunk = con.recv(40000)
+            fragments.append(chunk)
+                
+        ret = con.send(str.encode("ACK from server"))
+        if ret == 0:
+            raise RuntimeError("socket connection broken")
+        print ("[4] ACK is just sent to client..")
+
+        PORT += 1
+        con.close()
+        soc.close()
+
+
+except socket.error as err:
+    print(f"{str(err)}")
